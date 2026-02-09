32647740bc5bc3fde84460761dac23ed51f9512c
juliavaz11 <juliasntsvaz@gmail.com>
2026-02-09 10:44:19 -0300

    Atualizando a página Acoes da Connect

--- src/components/QuemSomos/QuemSomos.jsx
+++ src/components/QuemSomos/QuemSomos.jsx
@@ -4,21 +4,16 @@ import Background from "../../assets/backgroundQuemSomos.png";
 function QuemSomos() {
   return (
     <section className={styles.quemSomos}>
-      <img
-        className={styles.imgBackground}
-        src={Background}
-        alt="Imagem de pessoas reunidas"
-      />
-      <div className={styles.textoQuemSomos}>
+      <div className={styles.boxTexto}>
         <h1>Como a Connect atua em projetos sociais</h1>
         <p>
-          A Connect é uma iniciativa que conecta empresas, pessoas e organizações sociais com um propósito em comum: gerar impacto positivo e transformar realidades. Atuamos no desenvolvimento e apoio a projetos sociais nas áreas de educação, inclusão, tecnologia, esporte e assistência social, sempre com foco em impacto real e sustentável.
+          A Connect é uma iniciativa que conecta empresas, pessoas e organizações sociais com um propósito em comum: gerar impacto positivo e transformar realidades. Atuamos no desenvolvimento e apoio a projetos sociais nas áreas de educação, inclusão, tecnologia, esporte e assistência social, sempre com foco em impacto real e sustentável. </p>
 
-Há anos, trabalhamos aproximando quem quer ajudar de quem precisa, promovendo ações sociais estruturadas, transparentes e alinhadas às necessidades das comunidades. Acreditamos que a transformação acontece quando propósito, colaboração e responsabilidade caminham juntos.
+<p>Há anos, trabalhamos aproximando quem quer ajudar de quem precisa, promovendo ações sociais estruturadas, transparentes e alinhadas às necessidades das comunidades. Acreditamos que a transformação acontece quando propósito, colaboração e responsabilidade caminham juntos.</p>
 
-Nosso compromisso é conectar boas causas a quem deseja fazer a diferença, criando pontes entre empresas, voluntários e projetos sociais que realmente transformam vidas.
+<p>Nosso compromisso é conectar boas causas a quem deseja fazer a diferença, criando pontes entre empresas, voluntários e projetos sociais que realmente transformam vidas.
         </p>
-      </div>
+        </div>
     </section>
   );
 }
